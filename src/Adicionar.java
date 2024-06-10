cfa5cb723a3eb115355a8f55afc48833917c32d5
Mariana <marianabatistella3@gmail.com>
2024-06-10 20:32:01 -0300

    Primeiro commit

---
+++ src/Adicionar.java
@@ -0,0 +1,21 @@
+public class Adicionar {
+    String nome;
+    int idade;
+
+    public addPessoa (String nome, int idade){
+        this.nome = nome;
+        this.idade = idade; 
+    }
+
+        public void getIdade() {
+            return idade;
+        }
+
+        public void setIdade(int idade){
+            this.idade = idade;
+        }
+
+        public String toString(){
+            return "nome: " + ""
+        }
+}
---
+++ src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+       System.out.println("======== CADASTRO =======");
+       Scanner leitor = new Scanner(System.in);
+
+       ""
+
+    }
+}
